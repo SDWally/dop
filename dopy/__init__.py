ed0b7523ec336ee6fd9627272443f4bfb5eb1419
langirl <1161512420@qq.com>
2018-09-29 00:34:33 +0800

    init dopy

---
+++ dopy/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+# 用途： 学习python文档记录
+# 创建日期: 18-9-29 上午12:06
+import time
+
+if __name__ == "__main__":
+    start_t = time.time()
+
+    pass
+
+    print("use time: %s" % (time.time() - start_t))
