394a7a16232ef9d8639b71ef830620b90fb84a46
langirl <1161512420@qq.com>
2018-09-29 01:04:52 +0800

    update docs name

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Dopy'
+project = 'Dop'
 copyright = '2018, Wally'
 author = 'Wally'
 
@@ -101,7 +101,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'Dopydoc'
+htmlhelp_basename = 'Dopdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -128,7 +128,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'Dopy.tex', 'Dopy Documentation',
+    (master_doc, 'Dop.tex', 'Dop Documentation',
      'Wally', 'manual'),
 ]
 
@@ -138,7 +138,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'dop', 'Dopy Documentation',
+    (master_doc, 'dop', 'Dop Documentation',
      [author], 1)
 ]
 
@@ -149,8 +149,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'Dopy', 'Dopy Documentation',
-     author, 'Dopy', 'One line description of project.',
+    (master_doc, 'Dop', 'Dop Documentation',
+     author, 'Dop', 'One line description of project.',
      'Miscellaneous'),
 ]
 
--- docs/index.rst
+++ docs/index.rst
@@ -1,9 +1,9 @@
-.. Dopy documentation master file, created by
+.. Dop documentation master file, created by
    sphinx-quickstart on Sat Sep 29 00:31:58 2018.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to Dopy's documentation!
+Welcome to Dop's documentation!
 ================================
 
 .. toctree::
