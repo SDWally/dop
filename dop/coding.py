0a46443fb4832b46f6c1cee7e8f8ba31e995b0ba
Wally <1161512420@qq.com>
2019-08-03 22:33:34 +0800

    updat

--- dop/coding.py
+++ dop/coding.py
@@ -12,7 +12,28 @@ class Vector2d:
     >>> v1 = Vector2d(3, 4)
     >>> print(v1.x, v1.y)
     3.0 4.0
+    >>> v1
+    Vector2d(3.0, 4.0)
+    >>> v1_clone = eval(repr(v1))
+    >>> v1 == v1_clone
+    True
+    >>> print(v1)
+    (3.0, 4.0)
+    >>> octets = bytes(v1)
+    >>> octets
+    b'd\\x00\\x00\\x00\\x00\\x00\\x00\\x08@\\x00\\x00\\x00\\x00\\x00\\x00\\x10@'
+    >>> abs(v1)
+    5.0
+    >>> bool(v1), bool(Vector2d(0, 0))
+    (True, False)
+    >>> format(v1)
+    '(3.0, 4.0)'
+    >>> format(v1, '.2f')
+    '(3.00, 4.00)'
+
     """
+
+    typecode = "d"
 
     def __init__(self, x, y):
         self.x = float(x)
@@ -23,8 +44,7 @@ class Vector2d:
 
     def __repr__(self):
         class_name = type(self).__name__
-
-        return '{}({!r}, {!r}'.format(class_name, *self)
+        return '{}({!r}, {!r})'.format(class_name, *self)
 
     def __str__(self):
         return str(tuple(self))
@@ -41,8 +61,17 @@ class Vector2d:
     def __bool__(self):
         return bool(abs(self))
 
+    def __format__(self, format_spec=''):
+        if format_spec.endswith("p"):
+            format_spec = format_spec[:-1]
+            coords = (abs(self), self.angle())
+            outer_fmt = '<{}, {}>'
+        else:
+            coords = self
+            outer_fmt = '({}, {})'
+        components = (format(c, format_spec) for c in coords)
+        return outer_fmt.format(*components)
 
-
-
-
+    def angle(self):
+        return math.atan2(self.y, self.x)
 
